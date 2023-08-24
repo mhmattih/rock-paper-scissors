d98a3ccebe6214d338dec970b1f842f6885a91fe
Matti Hirvonen <60670065+mhmattih@users.noreply.github.com>
2023-08-24 17:37:26 +0300

    Create a function called getComputerChoice

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Rock Paper Scissors game begins!");
+
+// getComputerChoice
+// Compute computer's choice randomly, three choices 
+// Returns result string "Rock", "Paper" or "Scissors"
+function getComputerChoice(){
+    const computerChoices = ["Rock", "Paper", "Scissors"];
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+console.log("Computer's choice is: ",getComputerChoice());
